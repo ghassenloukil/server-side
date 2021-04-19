e0bc8f73bce5e50888cd13ea416a8c2ab795b7b0
ghassenloukil <gaston.loukil@gmail.com>
2021-04-19 22:18:42 +0200

    authentication

--- server/app/routes/auth.routes.js
+++ server/app/routes/auth.routes.js
@@ -19,5 +19,14 @@ module.exports =function(app){
         controller.signup
     );
 
+    app.post(
+        "/api/auth/signup",
+        [
+            verifySignUp.checkDuplicateUsernameOrEmail,
+            verifySignUp.checkRolesExisted
+        ],
+        controller.signup
+    );
+
     app.post("/api/auth/signin", controller.signin);
 };
