80a5dc76447732ad58c598dff6d8a819894f2afe
ghassenloukil <gaston.loukil@gmail.com>
2021-04-19 22:29:33 +0200

    fixing auth

--- server/app/routes/auth.routes.js
+++ server/app/routes/auth.routes.js
@@ -20,6 +20,13 @@ module.exports =function(app){
     );
 
 
-
+    app.post(
+        "/api/auth/signup",
+        [
+            verifySignUp.checkDuplicateUsernameOrEmail,
+            verifySignUp.checkRolesExisted
+        ],
+        controller.signup
+    );
     app.post("/api/auth/signin", controller.signin);
 };
