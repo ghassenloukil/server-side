7ce88064c4048e8284e00564e341f5bfd48f7694
ghassenloukil <gaston.loukil@gmail.com>
2021-04-21 01:20:53 +0200

    step 2

--- server/dataBase/index.js
+++ server/dataBase/index.js
@@ -3,14 +3,3 @@ var mysqlConfig = require('./config.js')
 
 var connection = mysql.createConnection(mysqlConfig)
 
-const getAllUsers = (req, res) => {
-    connection.query("SELECT * FROM user", (err, response) => {
-        if(err) {
-            console.log(err);
-        }else {
-            res.send(res)
-        }
-    })
-}
-
-module.exports = getAllUsers
